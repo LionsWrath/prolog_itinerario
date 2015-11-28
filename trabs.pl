0133a30c5f7a1d85f78d6fa77341bab8b5c08920
Adriano Cardoso <adriano_cardoso93@hotmail.com>
2015-11-28 00:58:36 -0200

    Update trabs.pl

--- trabs.pl
+++ trabs.pl
@@ -1,1 +1,15 @@
+:- use_module(library(pio)).
 
+program :-
+	open('Desktop/Prolog/dados.txt', read, X),
+	read_file(X, Lines),
+	close(X),
+	write(Lines).
+
+read_file(Stream,[]) :-
+    at_end_of_stream(Stream).
+
+read_file(Stream,[X|L]) :-
+    \+ at_end_of_stream(Stream),
+    read(Stream,X),
+    read_file(Stream,L).
