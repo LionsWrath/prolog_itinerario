aa6ad58e699c062381d1b2d03e0d7af536b4acf5
lionswrath <caio.tonetti@gmail.com>
2015-11-29 21:28:20 -0200

    busca melhorada

--- trabs.pl
+++ trabs.pl
@@ -1,13 +1,44 @@
 :- use_module(library(pio)).
 
-:- ['Desktop/Prolog/dados.pl'].
+:- ['dados/dados_teste.pl'].
 
-pisoMinimo(P) :-
-	dados_via(Cod, _, _, _, caracteristicas(X, _, _)),
-	X >= P,
-	write(Cod).
+pisoMinimo(Z,P) :-
+	Z >= P.
 
-connected(X,Y,L) :- dados_via(_, X, Y, L, caracteristicas(_, _, _)) ; dados_via(_, Y, X, L, caracteristicas(_, _, _)).
+%----Verifica se esta conectado
+connected(X,Y,L) :-
+	dados_via(_,X,Y,L,caracteristicas(_,_,_))
+	;
+	dados_via(_,Y,X,L,caracteristicas(_,_,_)).
+
+%----Verifica se esta conectado e o piso e suficientemente bom
+connected(X,Y,P,L) :-
+	dados_via(_, X, Y, L, caracteristicas(Z, _, _)),
+	pisoMinimo(Z,P)
+	;
+	dados_via(_, Y, X, L, caracteristicas(Z, _, _)),
+	pisoMinimo(Z,P).
+
+path(A,B,V,Path,Len) :-
+	travel(A,B,[A],V,Q,Len),
+	reverse(Q,Path).
+
+travel(A,B,P,V,[B|P],L) :-
+	connected(A,B,V,L).
+
+travel(A,B,Visited,V,Path,L) :-
+	connected(A,C,V,D),
+	C \== B,
+	\+ member(C, Visited),
+	travel(C,B,[C|Visited],V,Path,L1),
+	L is D+L1.
+
+shortest(A,B,V,Path,Lenght) :-
+	setof([P,L],path(A,B,V,P,L),Set),
+	Set = [_|_],
+	minimal(Set,[Path,Lenght]).
+
+%----
 
 path(A,B,Path,Len) :-
 	travel(A,B,[A],Q,Len),
